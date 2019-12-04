e0f7917663cd9d71a2c2effc597c9ce2b3146d7e
jobin <jobin.chen@newtype.io>
2019-12-04 14:38:15 +0800

    修改缓存中间件为只提供给/api/data接口

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const Cache = require('./middleware/cache')
+
 const CORS = require('./middleware/cors')
 const router = require('./routers/index')
 const cron = require('node-cron')
 const indexDate = require('./model/index')
+var Koa=require('koa');
 
+//每周凌晨更新数据
 cron.schedule('0 0 0 * * *', () => {
    indexDate.data.time  = new Date();
    console.log(`数据更新了:${indexDate}`)
  });
-var Koa=require('koa');
 
 //实例化
 var app=new Koa();
@@ -16,9 +17,6 @@ var app=new Koa();
 //处理跨域
 app.use(CORS)
 
-
-// 缓存处理中间件
-app.use(Cache(86400))
 
 //路由挂载
 app.use(router.routes())
--- routers/home.js
+++ routers/home.js
@@ -1,6 +1,8 @@
 const home = require('koa-router')()
+const Cache = require('../middleware/cache')
 const indexDate = require('../model/index')
-home.get('/',async (ctx)=>{
+// 缓存处理中间件
+home.get('/data',Cache(86400),async (ctx)=>{
  ctx.status = 200
   ctx.body = indexDate
 })
